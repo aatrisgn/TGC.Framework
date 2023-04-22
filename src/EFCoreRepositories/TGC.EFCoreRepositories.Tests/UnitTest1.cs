6f36d6355b57e8d644f14443b92598e5a999cfdf
Asger Arnbjørn Thyregod <asger.thyregod@gmail.com>
2023-04-22 14:22:44 +0200

    Added test projects to all packages

---
+++ src/EFCoreRepositories/TGC.EFCoreRepositories.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TGC.EFCoreRepositories.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
