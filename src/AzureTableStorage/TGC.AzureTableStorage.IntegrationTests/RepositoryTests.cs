8f1139134d3c54f0b70dfcc79768f0f63534e2ef
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2025-02-02 10:36:57 +0100

    Removed key (#5)

--- src/AzureTableStorage/TGC.AzureTableStorage.IntegrationTests/RepositoryTests.cs
+++ src/AzureTableStorage/TGC.AzureTableStorage.IntegrationTests/RepositoryTests.cs
@@ -14,7 +14,7 @@ public class RepositoryTests
         IServiceCollection serviceCollection = new ServiceCollection();
         serviceCollection.AddAzureTableStorage(configuration =>
         {
-            configuration.AccountConnectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+            configuration.AccountConnectionString = "";
             configuration.StubServices = false;
             configuration.UseManagedIdentity = false;
         });
