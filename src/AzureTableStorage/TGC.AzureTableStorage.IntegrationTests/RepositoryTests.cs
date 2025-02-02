1a2311269edd0f6edc319c92d8cdbecd11afe05e
Asger Thyregod <asger.thyregod@gmail.com>
2025-02-02 11:44:04 +0100

    Re-included tests

--- src/AzureTableStorage/TGC.AzureTableStorage.IntegrationTests/RepositoryTests.cs
+++ src/AzureTableStorage/TGC.AzureTableStorage.IntegrationTests/RepositoryTests.cs
@@ -41,10 +41,69 @@ public class RepositoryTests
         
         singleItem.Name.Should().Be("Test value");
     }
+    
+    [Fact]
+    public async Task GIVEN_ExistingItemInStorage_THEN_ItIsPossibleToRetrieveAnEntityWithAllProperties()
+    {
+        var repository = _serviceProvider.GetService<IAzureTableStorageRepository<TestEntity>>();
+
+        if (repository is null)
+        {
+            throw new NullReferenceException("repository is null and shouldn't be null.");
+        }
+        
+        await repository.CreateAsync(new TestEntity
+        {
+            Name = "Test value"
+        });
+        
+        var locatedItem = await repository.GetSingleAsync(e => e.Name == "Test value");
+        locatedItem.Name.Should().Be("Test value");
+    }
+    
+    [Fact]
+    public async Task GIVEN_ExistingItemInStorage_THEN_ItIsPossibleToRetrieveAllEntitiesWithAllProperties()
+    {
+        var repository = _serviceProvider.GetService<IAzureTableStorageRepository<TestEntity>>();
+
+        if (repository is null)
+        {
+            throw new NullReferenceException("repository is null and shouldn't be null.");
+        }
+        
+        await repository.CreateAsync(new TestEntity
+        {
+            Name = "Test value1",
+            Score = 1.2
+        });
+        
+        await repository.CreateAsync(new TestEntity
+        {
+            Name = "Test value2",
+            Score = 12.3
+        });
+        
+        await repository.CreateAsync(new TestEntity
+        {
+            Name = "Test value3",
+            Score = 123.4
+        });
+        
+        var locatedItem = await repository.GetAllAsync(e => true);
+        
+        locatedItem.Any(e => e.Name == "Test value1").Should().BeTrue();
+        locatedItem.Any(e => e.Name == "Test value2").Should().BeTrue();
+        locatedItem.Any(e => e.Name == "Test value3").Should().BeTrue();
+        
+        locatedItem.Any(e => e.Score == 1.2).Should().BeTrue();
+        locatedItem.Any(e => e.Score == 12.3).Should().BeTrue();
+        locatedItem.Any(e => e.Score == 123.4).Should().BeTrue();
+    }
 
     [TableItem("TestEntities")]
-    private class TestEntity : AzureTableItem, ITableEntity
+    private class TestEntity : AzureTableItem
     {
         public string Name { get; set; }
+        public double Score { get; set; }
     }
 }
